8d91449faccaaec9c06c7cb61984b50018fdca43
sellah <mailto:sellah.hajar16@gmail.com>
2023-02-20 16:51:05 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: 'get the last degiti of a number'
+  *Return: always 0
+  */
+int main(void)
+{
+	char c;
+	c = 'a';
+        while
+		(c <= 'z') {
+			putchar(c);
+	                c++;
+	}
+putchar('\n');
+return (0);
+}
