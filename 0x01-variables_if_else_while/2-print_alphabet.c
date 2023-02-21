35a1f9165e598d6f32a39690b6e904c85faabc4c
sellah <mailto:sellah.hajar16@gmail.com>
2023-02-20 17:09:24 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,16 @@
 int main(void)
 {
 	char c;
+
 	c = 'a';
-        while
-		(c <= 'z') {
-			putchar(c);
-	                c++;
+
+while
+	(c <= 'z')
+	{
+		putchar(c);
+		c++;
 	}
+
 putchar('\n');
 return (0);
 }
